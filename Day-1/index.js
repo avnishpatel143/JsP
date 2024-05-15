30d4b1290f2526575f08d1fe5bb9ea2af329fef7
Avnish Patel <avnishpatel855@gmail.com>
2024-05-15 19:09:07 +0530

    this commit EOD

--- Day-1/index.js
+++ Day-1/index.js
@@ -1,6 +1,17 @@
+fullName = "Tony stark"
+age = 24;
+radius = 14;
+a = null;
+y = undefined;
+console.log(y);
 
-
-function cubes(a){
-    return a * a * a;
-}
-console.log(cubes(4));
+// creating object 
+const profile = { 
+uesername: "shardhakapara",
+Followers: 569,
+follwing : 4,
+Name     : "Shard",
+Follow   : false,
+massages : undefined,
+};
+console.log(profile);
---
+++ Day-2/day-2.js
@@ -0,0 +1,1 @@
+console.log("day-2");
