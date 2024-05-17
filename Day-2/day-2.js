aa0479072e423f51abc955d5d4493194fa3c42db
Avnish Patel <avnishpatel855@gmail.com>
2024-05-17 18:36:52 +0530

    commit End of the day

--- Day-2/day-2.js
+++ Day-2/day-2.js
@@ -1,1 +1,16 @@
-console.log("day-2");
+// Aritmethic Oprater
+let a = 4;
+let b = 8;
+console.log("a = ",a, "& b = " ,b);
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b);
+console.log("a ** b =", a ** b);
+
+//Unary Operator
+
+
+
+
