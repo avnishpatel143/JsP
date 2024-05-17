a87b69a4f6f44713af61941da90a4be2f452fade
Avnish Patel <avnishpatel855@gmail.com>
2024-05-17 13:03:44 +0530

    Compleated the Day-1

--- Day-1/index.js
+++ Day-1/index.js
@@ -1,17 +1,21 @@
-// fullName = "Tony stark"
-// age = 24;
-// radius = 14;
-// a = null;
-// y = undefined;
-// console.log(y);
 
-// creating object 
-const profile = { 
-uesername: "shardhakapara",
-Followers: 569,
-follwing : 4,
-Name     : "Shard",
-Follow   : false,
-massages : undefined,
+const profile = {
+  uesername: "shardhakapara",
+  Followers : 569+"K",
+  follwing: 4,
+  Name : "Shard",
+  Follow : false,
+  massages : undefined,
 };
 console.log(profile);
+
+// we can store also in stintg with number like this (using +)
+//inko acces krne ke liyee .(dot) se kr sakte hai console me example- avnish.username || output will be = @avnishpatel143
+
+const avnish ={
+    username: "@avnishpatel"+143,
+    follwing: 2600,
+    Name: "Avnish Patel",
+    
+};
+console.log(avnish);
