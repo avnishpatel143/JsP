b2023a024319cb09fa02c08922f50e5421da0be4
Avnish Patel <avnishpatel855@gmail.com>
2024-05-20 15:15:57 +0530

    IMPORTANT MERN Notes

--- Day-2/day-2.js
+++ Day-2/day-2.js
@@ -1,15 +1,37 @@
 // Aritmethic Oprater
-let a = 4;
-let b = 8;
-console.log("a = ",a, "& b = " ,b);
-console.log("a + b =", a + b);
-console.log("a - b =", a - b);
-console.log("a * b =", a * b);
-console.log("a / b =", a / b);
-console.log("a % b =", a % b);
-console.log("a ** b =", a ** b);
+// let a = 4;
+// let b = 8;
+// console.log("a = ",a, "& b = " ,b);
+// console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+// console.log("a % b =", a % b);
+// console.log("a ** b =", a ** b);
 
 //Unary Operator
+// let a = 4;
+// let b = 8;
+// console.log("++a =", ++a);
+// console.log("a=", a);
+
+
+// //Assignment Oprators- = += -= %= **=
+
+// let c = 9;
+// let d = 5;
+// d+= 4;          // which means d = d + 4
+// console.log("d = ", d);
+
+
+
+//Comprison Oprators-
+
+let a = 4;
+let b = 4;
+console.log("!a=", a != b);
+
+
 
 
 
