8a32fb25ce31012f3333bee5c91bd8db13c32b18
Avnish Patel <avnishpatel855@gmail.com>
2024-05-24 16:52:26 +0530

    Changes of the last

--- FreeCodeChamp/js.js
+++ FreeCodeChamp/js.js
@@ -20,3 +20,9 @@ firstLetterOfLastName = lastName[0]; // Change this line
 
 console.log(lastName[0]);  //for my understanig 
 
+// Correct the assignment to myStr so it contains the string value of Hello World using the approach shown in the example above.
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
