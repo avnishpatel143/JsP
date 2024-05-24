b4a31bcc942e203639495a29ffbdf69fd770334a
Avnish Patel <avnishpatel855@gmail.com>
2024-05-24 14:01:50 +0530

    Added the FreeCodeChamp after solving questions and answers

---
+++ FreeCodeChamp/js.js
@@ -0,0 +1,22 @@
+ // Use the .length property to set lastNameLength to the number of characters in lastName.
+
+// Setup
+let lastNameLength = 0;
+// uncomment before use this const  lastName = "Lovelace"; 
+
+// Only change code below this line
+console.log(lastName.length)
+lastNameLength = lastName.length
+console.log(lastNameLength)
+
+
+// Use bracket notation to find the first character in the lastName variable and assign it to firstLetterOfLastName.
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+console.log(lastName[0]);  //for my understanig 
+
