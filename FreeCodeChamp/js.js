7c9cf2b23f34b762c5357c70154897cd8db24c35
Avnish Patel <avnishpatel855@gmail.com>
2024-05-27 12:25:25 +0530

    Sovleing the Questons of FreeCodeChamp Questions

--- FreeCodeChamp/js.js
+++ FreeCodeChamp/js.js
@@ -26,3 +26,43 @@ let myStr = "Jello World";
 
 // Only change code below this line
 myStr = "Hello World"; // Change this line
+
+
+/* Let's try to set thirdLetterOfLastName to equal the third letter of the lastName variable using bracket notation.
+
+Hint: Try looking at the example above if you get stuck.*/
+
+// Setup
+const lastName1 = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName1[2]; // Change this line
+
+console.log(thirdLetterOfLastName); // output will be v
+
+
+/* Use bracket notation to find the last character in the lastName variable.*/
+
+// Setup
+const lastName0 = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName0.length - 1]; // Change this line
+ 
+ console.log(lastLetterOfLastName);   // the output will be =  e
+ console.log(lastName);                // the output will be = Lovelace
+
+
+ // Use bracket notation to find the second-to-last character in the lastName string.
+
+
+ // Setup
+const lastName3 = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName3.length - 2]; // Change this line
+
+console.log(secondToLastLetterOfLastName); // the output will be = c
+
+
+
