4375739a40de6a3e569a2c65398bbea1c03cf130
Avnish Patel <avnishpatel855@gmail.com>
2024-05-28 17:40:47 +0530

    push of the end of the day

--- FreeCodeChamp/js.js
+++ FreeCodeChamp/js.js
@@ -104,3 +104,20 @@ const myArrayA = [50, 60, 70];
 const myData = myArrayA[0]
 
 console.log(myData);  //The output will be =  50 
+
+ // Using bracket notation select an element from myArray such that myData is equal to 8.
+
+ const myArray0 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData0 = myArray0[2][1];
+  console.log(myData0) // output will be = 8
+
+
+  
+
+
