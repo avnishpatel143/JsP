21329a03972c9fe13b0ca15a031d9830800ed9d1
Avnish Patel <avnishpatel855@gmail.com>
2024-06-21 14:12:49 +0530

    Finshed the Js Basic IV

--- Love_Js_Basic4/BasicIV.js
+++ Love_Js_Basic4/BasicIV.js
@@ -52,23 +52,86 @@ return total;
  // issue of this function it is only read function 
 
 
- let person = {
- name : 'Avnish',
- fName : 'Patel',
- age : 32,
- Id : 420,
-//  Fruits: ['apple','graps','suje','Mango' ],
+//  let person = {
+//  name : 'Avnish',
+//  fName : 'Patel',
+//  age : 32,
+//  Id : 420,
 
- get fullName(){
+//  get fullName(){
 
-    return `${this.person.name} ${this.person.fName}`;
- },
+//     return `${this.person.name} ${this.person.fName}`;
+//  },
 
-set fullName(value) {
-    
-    let parts = value.split('');
-    this.name = parts[0];
-    this.Id = parts[1];
+// set fullName(value) {
+//     if (typeof value !== 'string')
+//     throw new Error ('not a String');
+//     let parts = value.split('');
+//     this.name = parts[0];
+//     this.Id = parts[1];
+// }
+//  };
+//  console.log(person.fullName);
+
+
+//  // Try & Cath - Error Handling 
+// try{
+//    person.fullName = true;
+
+// }
+// catch(e){
+//    alert('You have sent number in fullName');
+// }
+// console.log(person.fullName)
+
+// alert(e);
+
+
+
+// Scope:- Ek variable ka life time hota hai wahe scope hota hai , var variable we can acees in entire sheet even if assign in function also 
+//and let variable are block scope it is can access only in blocks cots // And the Const like work scope Var // 
+
+// example 
+
+
+function a(){
+  const ab = 3;
+
 }
- };
- console.log(person.fullName);
+function b(){
+    const ab = 7;
+}
+
+a();
+b();
+
+
+for (var i = 0; i<10; i++){
+    
+}
+console.log(i);
+
+
+if (true){
+    var r = 20;
+}
+console.log(a);
+
+
+// Reducing the Arry with the two method the first one in for loop and second one is Reduce function
+
+// Adding the value of the arry using the for of loop 
+
+let arr = [1,3,3,5];
+// let total = 0;
+
+// for(let value of arr)
+//  total = total + value;d
+// console.log(total);
+
+// This is the Reduce method adding the value of the arry 
+
+let totalSum = arr.reduce((accumlator, currentValue) => accumlator + currentValue ,0);
+console.log("Printing sum");
+console.log(totalSum);
+
