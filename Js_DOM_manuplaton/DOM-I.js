1b0d8a506971916bfd3c4d26d2d26d669e722a40
Avnish Patel <avnishpatel855@gmail.com>
2024-07-04 12:13:43 +0530

    added the event listner for click

--- Js_DOM_manuplaton/DOM-I.js
+++ Js_DOM_manuplaton/DOM-I.js
@@ -1,7 +1,12 @@
-let mypara = document.createElement('p');
-mypara.textContent = 'I am the text content';
-content.appendChild(mypara);
-console.log(mypara);
+// let mypara = document.createElement('p');
+// mypara.textContent = 'I am the text content';
+// content.appendChild(mypara);
+// console.log(mypara);
 
 
+document.getElementById('conetnt');
 
+conetnt.addEventListener('click', function(){
+    conetnt.style.background = 'red';
+  console.log('Clicked');
+});
