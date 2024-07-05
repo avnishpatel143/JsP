dce678c207d1e7b1c5271198d2cf96eed879d635
Avnish Patel <avnishpatel855@gmail.com>
2024-07-05 13:24:04 +0530

    Added the JavaScript DOM 3rd class

--- Js_DOM_manuplaton/DOM-II/DOM-II.js
+++ Js_DOM_manuplaton/DOM-II/DOM-II.js
@@ -1,25 +1,24 @@
-// document.getElementById('conetnt');
+document.getElementById('conetnt');
 
-// function print (){
-//     conetnt.style.background = 'green';
-//   console.log('Clicked');
-// };
+function print (){
+    conetnt.style.background = 'green';
+  console.log('Clicked');
+};
 
-// conetnt.addEventListener('click', print);
-
-
-
-// conetnt.removeEventListener('click',print);
-// // conetnt.style.background = 'red' fddfd
+conetnt.addEventListener('click', print);
 
 
-// const   content = document.querySelector('#idd');
 
-// content.addEventListener( 'click', function(avnish){
+conetnt.removeEventListener('click',print);
+// conetnt.style.background = 'red' fddfd
+
+
+const   content = document.querySelector('#idd');
+
+content.addEventListener( 'click', function(avnish){
  
-//   console.log( avnish);
-// });
-
+  console.log( avnish);
+});
 
  const links = document.querySelectorAll('a');
  let fourthLinks = links[3];
@@ -29,6 +28,7 @@
     event.preventDefault();
     alert("Abb 4th links hamne rok diya")
   });
+  
 
 
 
---
+++ Js_DOM_manuplaton/DOM-III/index.js
@@ -0,0 +1,39 @@
+// adding 100 para 
+const t1 = performance.now();  // for the measure the code speed
+
+for (let i=1; i<=100; i++ ){
+
+let newElement = document.createElement('p');
+newElement.textContent = 'This is the Para  ' +  i;
+
+document.body.appendChild(newElement);
+};
+
+const t2 = performance.now();  // for the measure the code speed
+console.log( "this took" + (t1-t2) + "ms" );
+
+
+
+// optimising a bit 
+
+const t3 = performance.now();    // for the measure the code speed
+
+let myDiv = document.createElement('div');
+
+for (let i=1; i<=100; i++ ){
+
+    let element = document.createElement('p');
+    element.textContent = 'This is the Para' + 1;
+
+    myDiv.appendChild(element)
+    };
+    document.body.appendChild(myDiv);
+
+    const t4 = performance.now();    // for the measure the code speed 
+
+    console.log( "this took" + (t3-t4) + "ms" );   // this for the measure speed how much take time to run this code 
+
+
+
+
+    
