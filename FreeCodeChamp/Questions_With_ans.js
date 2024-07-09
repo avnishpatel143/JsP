dea7923b627faf10941d463c46389ad327fd0801
Avnish Patel <avnishpatel855@gmail.com>
2024-07-09 11:54:34 +0530

    I am the back for Compliting the prvious questions

--- FreeCodeChamp/Questions_With_ans.js
+++ FreeCodeChamp/Questions_With_ans.js
@@ -833,3 +833,44 @@ console.log(answer);              // the output will be stuff bcz default valu s
 console.log(switchOfStuff('a'));  // aise bhe console krwa sakte hai 
 
  
+
+
+/* Write a switch statement to set answer for the following ranges:
+1-3 - Low
+4-6 - Mid
+7-9 - High
+
+Note: You will need to have a case statement for each number in the range. */
+
+// This the answer 
+
+function sequentialSizes(val) {
+  let answer = "";
+   // Only change code below this line
+    switch (val){
+      case 1:
+      case 2:
+      case 3:  
+   
+    answer = "Low";
+   break;
+      case 4:
+      case 5:
+      case 6:
+ answer = "Mid";  
+ break;
+    case 7:
+    case 8:
+    case 9:
+ 
+  answer = "High"; 
+   }
+  // Only change code above this line
+  return answer;
+};
+
+sequentialSizes(1);
+
+let a = sequentialSizes(1)  
+
+console.log(a);  // the output will be Low 
