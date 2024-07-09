a54bbe1df6c542d65bfbee0a7d483a407bd921d8
Avnish Patel <avnishpatel855@gmail.com>
2024-07-09 19:23:21 +0530

    solved 80% questions

--- FreeCodeChamp/Questions_With_ans.js
+++ FreeCodeChamp/Questions_With_ans.js
@@ -874,3 +874,63 @@ sequentialSizes(1);
 let a = sequentialSizes(1)  
 
 console.log(a);  // the output will be Low 
+
+
+
+//Change the chained if/else if statements into a switch statement.
+
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+
+  switch (val){
+  case "bob":
+     answer ="Marley"; 
+     break;
+  case 42:
+     answer = "The Answer";
+     break;
+  case 1:
+    answer ="There is no #1";
+    break;
+  case 99:
+    answer = "Missed me by this much!";
+    break;
+  case 7:
+  answer = "Ate Nine"
+ }
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
+
+
+
+
+// Fix the function isLess to remove the if/else statements.
+
+
+// Before =>>>>
+function isLess(a, b) {
+  // Only change code below this line
+  if (a < b) {
+    return true;
+  } else {
+    return false;
+  }
+  // Only change code above this line
+}
+
+isLess(10, 15);
+
+// After=>>
+function isLess(a, b) {
+  // Only change code below this line
+
+    return a <= b;
+
+  // Only change code above this line
+}
+
+isLess(10, 15);
