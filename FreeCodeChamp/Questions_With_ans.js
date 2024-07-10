0f76df5a68430392b8ec4fe33a187f25270eaf58
Avnish Patel <avnishpatel855@gmail.com>
2024-07-10 19:26:08 +0530

    Commit of the day

--- FreeCodeChamp/Questions_With_ans.js
+++ FreeCodeChamp/Questions_With_ans.js
@@ -1055,4 +1055,23 @@ console.log(shirtValue)  // The output will be jersey
 console.log(hatValue)  // The output will be ballcap
 
 
+// Question =>> 
+//Read the values of the properties an entree and the drink of testObj using bracket notation 
+//and assign them to entreeValue and drinkValue respectively.
+
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"];   // Change this line
+const drinkValue = testObj["the drink"];    // Change this line
+
+console.log(entreeValue)  // the outpur will be hamburger
+console.log(drinkValue)   // The output will he water 
+
+
 
