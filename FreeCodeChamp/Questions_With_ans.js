f7839104267108ff4ced63092ae3104d095be7a8
Avnish Patel <avnishpatel855@gmail.com>
2024-07-10 17:45:21 +0530

    I am not solve this question

--- FreeCodeChamp/Questions_With_ans.js
+++ FreeCodeChamp/Questions_With_ans.js
@@ -934,3 +934,81 @@ function isLess(a, b) {
 }
 
 isLess(10, 15);
+
+
+
+// Question  =>> Modify the function abTest so that if a or b are less than 0 
+//the function will immediately exit with a value of undefined.
+
+// Hint:-
+// Remember that undefined is a keyword, not a string.
+
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+if (a < 0 || b < 0){
+  return undefined;
+
+}
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);
+
+
+
+// You will write a card counting function. It will receive a card parameter, 
+//which can be a number or a string, and increment or decrement the global count variable according 
+//to the card's value (see table). The function will then return a string with the current count and the string Bet if the count is positive, 
+//or Hold if the count is zero or negative. The current count and the player's decision (Bet or Hold) should be separated by a single space.
+
+// Example Outputs: -3 Hold or 5 Bet
+
+// Hint
+// Do NOT reset count to 0 when value is 7, 8, or 9.
+// Do NOT return an array.
+// Do NOT include quotes (single or double) in the output.
+
+
+
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+switch(card) {
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+  count++;
+    break;
+  case 10:
+  case "J":
+  case "Q":
+  case "K":
+  case "A":
+  count--;
+    break;
+}
+
+if (count > 0)
+{
+return count + " Bet";
+}
+else if (count < 0) {
+  return count + " Hold";
+}
+
+else if(count == 0){
+  return count + " Hold";
+
+}
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
