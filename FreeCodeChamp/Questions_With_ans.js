e7949ac0de0747571f57ef0b4355575ba7ae0f03
Avnish Patel <avnishpatel855@gmail.com>
2024-07-10 18:42:16 +0530

    A

--- FreeCodeChamp/Questions_With_ans.js
+++ FreeCodeChamp/Questions_With_ans.js
@@ -1012,3 +1012,47 @@ else if(count == 0){
 }
 
 cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+
+
+// Question ==>
+// Make an object that represents a dog called myDog which contains the properties name (a string), legs, tails and friends.
+
+// You can set these object properties to whatever values you want, as long as name is a string, legs and tails are numbers, and friends is an array.
+
+
+// Answers =>>
+
+const myDog = {
+  // Only change code below this line
+  name:"",
+  "legs": 4,
+  "tails":  1,
+  friends: ['Human', 'other animals']
+
+  // Only change code above this line
+};
+
+
+// Question =>> Read in the property values of testObj using dot notation. 
+//Set the variable hatValue equal to the object's property hat and set the variable shirtValue equal 
+//to the object's property shirt.
+
+
+// Setup
+const testObj = {
+  hat: "ballcap",
+  shirt: "jersey",
+  shoes: "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
+
+
+console.log(shirtValue)  // The output will be jersey
+console.log(hatValue)  // The output will be ballcap
+
+
+
