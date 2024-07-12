5bd282df7c2aa23cbb0aaf146dcfa015f40ae506
Avnish Patel <avnishpatel855@gmail.com>
2024-07-12 16:36:50 +0530

    Aaj code ka mood nhe

--- FreeCodeChamp/Questions_With_ans.js
+++ FreeCodeChamp/Questions_With_ans.js
@@ -1056,3 +1056,21 @@ function phoneticLookup(val) {
 phoneticLookup("charlie");
 
 
+// Question =>>
+//Modify the function checkObj to test if the object passed to the function parameter obj contains the
+// specific property passed to the function parameter checkProp. If the property passed to checkProp is found on obj, 
+//return that property's value. If not, return Not Found.
+
+
+//Answer =>>
+
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+  if (obj.hasOwnProperty(checkProp)){
+
+   return obj[checkProp];    
+  }else {
+    return "Not Found";
+  }
+  // Only change code above this line
+}
