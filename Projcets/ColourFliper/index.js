15ac2294d613dc2996218f7030fc4fde5ce4cac4
Avnish Patel <avnishpatel855@gmail.com>
2024-07-16 18:45:03 +0530

    Now working on js

---
+++ Projcets/ColourFliper/index.js
@@ -0,0 +1,9 @@
+const targeBtn = document.getElementById('btn');
+const back =  document.getElementById('contenr');
+
+
+
+targeBtn.addEventListener('click',function() {
+   back.style.background = 'red';
+    
+});
