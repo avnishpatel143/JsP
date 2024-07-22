9efe0e1a9d6b6777de72c75f2f079e221f986042
Avnish Patel <avnishpatel855@gmail.com>
2024-07-22 18:54:17 +0530

    Solve 100+ question

--- FreeCodeChamp/Questions_With_ans.js
+++ FreeCodeChamp/Questions_With_ans.js
@@ -1236,3 +1236,28 @@ console.log(myArray);   // The output will be [ 9, 7, 5, 3, 1 ]
 
 
 
+
+ // Question =>> Modify function multiplyAll so that it returns the product of all the numbers in the sub-arrays of arr.
+
+ // Answer
+function multiplyAll(arr) {
+
+  let product = 1;
+
+  // Only change code below this line 
+
+  for (let i = 0; i < arr.length; i++) {
+
+    for (let j = 0; j < arr[i].length; j++) {
+
+      console.log(arr[i][j])
+
+      product = product * arr[i][j]
+
+    }
+  };
+  // Only change code above this line
+  return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
