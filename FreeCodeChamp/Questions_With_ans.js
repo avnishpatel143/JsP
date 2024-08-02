01ffccc3d72ec7142cb9de14f36ff17bbb1ea848
Avnish Patel <avnishpatel855@gmail.com>
2024-08-02 16:19:31 +0530

    Once again Solving the Slolving Questions

--- FreeCodeChamp/Questions_With_ans.js
+++ FreeCodeChamp/Questions_With_ans.js
@@ -1286,3 +1286,89 @@ do {
 
 
 
+
+  //Question =>>> Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
+
+  // Answer 
+  function sum(arr, n) {
+    // Only change code below this line
+    if(n <= 0) {
+      return 0;
+    } else {
+      return sum(arr, n - 1) + arr[n - 1];
+    }
+  
+    // Only change code above this line
+  }
+
+
+
+  // Questions=>>
+
+//   We have an array of objects representing different people in our contacts lists.
+
+// A lookUpProfile function that takes name and a property (prop) as arguments has been pre-written for you.
+
+// The function should check if name is an actual contact's firstName and the given property (prop) is a property of that contact.
+
+// If both are true, then return the "value" of that property.
+
+// If name does not correspond to any contacts then return the string No such contact.
+
+// If prop does not correspond to any valid properties of a contact found to match name then return the string No such property.
+
+
+
+// Answer =>>
+
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+
+ for (let i = 0; i < contacts.length; i++) {
+
+    if (contacts[i].firstName === name) {
+
+      if (prop in contacts[i]) {
+
+        return contacts[i][prop];
+
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+}
+  // Only change code above this line
+
+
+lookUpProfile("Akira", "likes");
