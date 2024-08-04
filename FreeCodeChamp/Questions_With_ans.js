8838b3ffb887296244bda3d55deaf215a6734df5
avnish patel <avnishpatel855@gmail.com>
2024-08-04 15:06:24 +0530

    Solved all the questios

--- FreeCodeChamp/Questions_With_ans.js
+++ FreeCodeChamp/Questions_With_ans.js
@@ -1434,3 +1434,101 @@ function convertToInteger(str) {
  
  
 "terminal.integrated.tabs.title": "${sequence}"
+
+
+
+//Question=>>
+// Use parseInt() in the convertToInteger function so it converts a binary number to an integer and returns it.
+
+// Answer of this question 
+ function convertToInteger(str) { 
+  return parseInt(str, 2);
+  }
+ console.log(convertToInteger("1010")); // Output: 10
+
+
+
+ // Question =>>
+ //Use the conditional operator in the checkEqual function to check if two numbers are equal or not. 
+ //The function should return either the string Equal or the string Not Equal.
+
+
+ // Answer will be
+
+ function checkEqual(a,b){
+
+return a == b ? "Equal" : "Not Equal";     // This is also if else condiction
+ };
+
+ console.log(checkEqual(1,1 ));  // output will be equal 
+
+ console.log(checkEqual(3,1 )); // The output will be Not Equal
+
+
+
+ // Question =>> 
+// In the checkSign function, use multiple conditional operators - following the recommended format used in findGreaterOrEqual -
+// to check if a number is positive, negative or zero. The function should return positive, negative or zero.
+
+// Answer 
+
+function checkSign(num) {
+
+  return num  > 0  ? "positive" :
+   num < 0 ? "negative" :"zero"; 
+             
+ }
+ 
+ console.log(checkSign(1));  // output positive
+ console.log(checkSign(-1));  // output negative
+ console.log(checkSign(0));   // output zero
+ 
+
+
+
+ //  Qusestion =>>
+
+//  We have defined a function called countdown with one parameter (n). The function should use recursion to return an array containing the integers n through 1 based on the n parameter. 
+//If the function is called with a number less than 1, the function should return an empty array. For example, calling this function with n = 5 should return the array [5, 4, 3, 2, 1].
+// Your function must use recursion by calling itself and must not use loops of any kind.
+
+
+// Answer ==>
+
+  // Only change code below this line
+function countdown(n){
+  if (n < 1) {
+    return [];
+  }else {
+    const count = countdown(n - 1);
+    count.unshift(n);
+    return count;
+  }
+}
+console.log(countdown(18));
+// Only change code above this line
+
+
+
+// Question =>>
+
+// We have defined a function named rangeOfNumbers with two parameters. The function should return an array 
+//of integers which begins with a number represented by the startNum parameter and ends with a number represented
+// by the endNum parameter. The starting number will always be less than or equal to the ending number. 
+//Your function must use recursion by calling itself and not use loops of any kind. It should also work for cases where both startNum and endNum are the same.
+
+
+ // Answer ==>>
+
+function rangeOfNumbers(startNum, endNum) {
+
+  if( endNum < startNum){
+     return [];
+
+  }else {
+    const number = rangeOfNumbers(startNum, endNum -1);
+    number.push(endNum);
+    return number;
+  }
+};
+
