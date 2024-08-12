a39320915f54d69d8477d2216110c40e5736388a
Avnish Patel <avnishpatel855@gmail.com>
2024-08-12 17:20:20 +0530

    Es6 solving

--- FreeCodeChamp/Es6/Es6Q&A.js
+++ FreeCodeChamp/Es6/Es6Q&A.js
@@ -89,3 +89,30 @@ const LOCAL_FORECAST = {
  // Only change code above this line
               
                             
+  // Question =>> 
+ // Use destructuring assignment to swap the values of a and b so that a receives the value stored in b, and b receives the value stored in a.
+
+// Answer >>
+let a = 8, b = 6;
+// Only change code below this line
+[a,b] = [b,a]
+
+
+//Question =>>
+//Use a destructuring assignment with the rest syntax to emulate the behavior of Array.prototype.slice(). removeFirstTwo() 
+//should return a sub-array of the original array list with the first two elements omitted.
+
+
+
+
+function removeFirstTwo(list) {
+
+  const [a,b, ...shorterList] = list;
+
+  return shorterList ;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
+
+console.log(sourceWithoutFirstTwo)  // the output will be [ 3, 4, 5, 6, 7, 8, 9, 10 ]
