551a14e11865e350bb33e9d665d9debe766aa515
Avnish Patel <avnishpatel855@gmail.com>
2024-08-28 16:27:53 +0530

    I am back for solving ES6

--- FreeCodeChamp/Es6/Es6Q&A.js
+++ FreeCodeChamp/Es6/Es6Q&A.js
@@ -139,3 +139,60 @@ const half = ({min,max}) => (max + min) / 2.0;
 
 // Only change code above this line
 
+
+
+
+// Question >>
+ 
+//Use template literal syntax with backticks to create an array of list element (li) strings. Each list element's text should be one of the array elements from the failure property on the result object and have a class attribute with the value text-warning. The makeList function should return the array of list item strings.
+
+//Use an iterator method (any kind of loop) to get the desired output (shown below).
+
+// [
+//   '<li class="text-warning">no-var</li>',
+//   '<li class="text-warning">var-on-top</li>',
+//   '<li class="text-warning">linebreak</li>'
+// ]
+
+
+
+// Answer =>>
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = [];
+   for(let i = 0; i < arr.length; i++){
+
+     failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+   }
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+
+console.log(failuresList)
+
+
+// Question =>>
+
+//Use object property shorthand with object literals to create and return an object with name, age and gender properties.
+
+const createPerson = (name, age, gender) => { 
+  // Only change code below this line
+  return {
+    name,
+    age ,
+    gender
+  };
+  // Only change code above this line
+};
+
+
+console.log(createPerson("aman",32 ,"male"))
