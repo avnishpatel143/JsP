a16e1578fa3a224a0cdb7bb6b1c8cb5d355d6d2c
Avnish Patel <avnishpatel855@gmail.com>
2024-09-30 11:23:45 +0530

    h

--- FreeCodeChamp/Es6/Es6Q&A.js
+++ FreeCodeChamp/Es6/Es6Q&A.js
@@ -315,3 +315,22 @@ subtract(7,4);
 const makeServerRequest = new Promise((resolve, reject ) => {
 
 });
+
+
+// Question >>
+//Make the promise handle success and failure. If responseFromServer is true, call the resolve method to successfully complete the promise.
+// Pass resolve a string with the value We got the data. If responseFromServer is false, use the reject method instead and pass it the string: Data not received.
+
+
+// Answer =>>
+const makeServerRequest2 = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+
